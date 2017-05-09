0bdf9321cb33348985a40b63a74a42d963788790
gaozhuo <gaozhuo@sf-express.com>
2017-05-09 20:24:31 +0800

    commit

--- JavaSecurity/JavaSecurityDemo/src/test/java/MyTest.java
+++ JavaSecurity/JavaSecurityDemo/src/test/java/MyTest.java
@@ -1,6 +1,9 @@
 import org.junit.Test;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.security.Provider;
+import java.security.SecureRandom;
 import java.security.Security;
 
 /**
@@ -10,7 +13,7 @@ import java.security.Security;
 public class MyTest {
 
     @Test
-    public final void myTest() throws Exception {
+    public final void testProvider() throws Exception {
 
         for (Provider provider : Security.getProviders()) {
 
@@ -22,6 +25,23 @@ public class MyTest {
             System.out.println("\n");
         }
 
+
+    }
+
+    @Test
+    public final void testKey() throws Exception {
+
+        //生成秘钥
+        String password = "testkey";
+
+        KeyGenerator kg = KeyGenerator.getInstance("AES");
+
+        //SecureRandom用来生成安全随机数序列，password.getBytes()是种子，只要种子相同，序列就一样，所以生成的秘钥就一样。
+        kg.init(128, new SecureRandom(password.getBytes()));
+
+        SecretKey key = kg.generateKey();
+
+        System.out.println(key.getEncoded());
 
     }
 
